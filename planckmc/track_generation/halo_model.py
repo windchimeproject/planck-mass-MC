396637b64c65b9ff686a03f2534cef28d5bd2f74
Juehang Qin <qinjuehang@hotmail.com>
2020-04-20 19:23:03 -0400

    update to fix njit on vel_calc

--- planckmc/track_generation/halo_model.py
+++ planckmc/track_generation/halo_model.py
@@ -13,11 +13,11 @@ def vel_make_cdf():
     integrated_vel_rate = sum([x*((max(v_vals)-min(v_vals))/len(v_vals)) for x, y in zip(vel_dist, v_vals)])
     vel_pdf = np.array(vel_dist)/integrated_vel_rate
     vel_cdf = [sum(vel_pdf[:i])/sum(vel_pdf) for i in range(len(vel_pdf))]
-    return v_vals, vel_cdf
+    return v_vals, np.array(vel_cdf)
 
 @njit
 def vel_calc(v_vals, vel_cdf):
-    rnd_unt_num_vel = np.random.uniform()
-    vel_index = np.argmin(abs(np.array(vel_cdf) - rnd_unt_num_vel))
+    rnd_unt_num_vel = np.random.rand()
+    vel_index = np.argmin(np.abs(vel_cdf - rnd_unt_num_vel))
     vel = v_vals[vel_index]
     return vel
--- planckmc/track_generation/make_tracks.py
+++ planckmc/track_generation/make_tracks.py
@@ -1,2 +1,5 @@
 import numpy as np
 from numba import njit
+import numericalunits as nu
+from ..config import CONFIG
+
