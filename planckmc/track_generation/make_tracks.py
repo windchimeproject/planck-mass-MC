c90bb01d12cbda28fa7b490ea67d2a8ed0f92d77
Juehang Qin <qinjuehang@hotmail.com>
2020-04-25 13:45:21 -0400

    skeleton generate_tracks() and loc. config params

--- planckmc/track_generation/make_tracks.py
+++ planckmc/track_generation/make_tracks.py
@@ -1,5 +1,13 @@
 import numpy as np
 from numba import njit
-import numericalunits as nu
 from ..config import CONFIG
 
+@njit
+def generate_tracks(vel, t_entry):
+    '''Return track with entry and exit vectors in local coords (x=north, y=east, z=up), and entry & exit time.
+    vel is an array of velocities. t_entry is an array of time.
+    generate_tracks(vel, t_entry):
+        return ((x_entry, y_entry, z_entry, time_exit), (x_exit, y_exit, z_exit, time_exit))
+    '''
+    radius = float(CONFIG['Track Generation']['BoundingSphereRadius'])
+    return ((0, 0, 1, t_entry), (0, 1, 0, t_entry))
