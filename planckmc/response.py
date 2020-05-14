5d39711e894eee322b59fcb36d6cb179786aa04f
rorland014 <rorlando@purdue.edu>
2020-05-14 18:58:12 -0400

    Added a layer that converts accelerations to an output voltage in response.py.

--- planckmc/response.py
+++ planckmc/response.py
@@ -5,19 +5,23 @@ import numpy as np
 from scipy import signal
 
 from .config import CONFIG
+from detector_characteristics import DETECTOR_CHARACTERISTICS
 
 RESPONSE_FILE = CONFIG['Detector Config']['ResponseFile']
 
 with open(RESPONSE_FILE) as f:
     RESPONSE_DICT = json.load(f)
-
-
+    
 def sensor_response(sensor, acceleration, response_dict=RESPONSE_DICT):
     '''returns ADC value based on true MC acceleration'''
+    g = 9.81
     linear_response = response_dict[sensor]['linear_response']
+    sensitivity = DETECTOR_CHARACTERISTICS[sensor]['sensitivity']
     convolved_list = []
     for dim in range(acceleration.shape[1]):
-        convolved_list.append(signal.convolve(acceleration[:, dim], linear_response))
+        acceleration[:, dim] /= g
+        voltage = np.dot(acceleration[:, dim], sensitivity)
+        convolved_list.append(signal.convolve(voltage, linear_response))
     convolved_signal = np.array(convolved_list).T
 
     signal_transfer_response = response_dict[sensor]['signal_transfer_response']
