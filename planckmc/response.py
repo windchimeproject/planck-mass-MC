dc783467a194e8ee216af1078f70b787280c04f1
Juehang Qin <qinjuehang@hotmail.com>
2020-05-15 12:43:33 -0400

    fix issues with response.py

--- planckmc/response.py
+++ planckmc/response.py
@@ -11,16 +11,15 @@ RESPONSE_FILE = CONFIG['Detector Config']['ResponseFile']
 
 with open(RESPONSE_FILE) as f:
     RESPONSE_DICT = json.load(f)
-    
+
 def sensor_response(sensor, acceleration, response_dict=RESPONSE_DICT):
     '''returns ADC value based on true MC acceleration'''
-    g = 9.81
+
     linear_response = response_dict[sensor]['linear_response']
     sensitivity = DETECTOR_CHARACTERISTICS[sensor]['sensitivity']
     convolved_list = []
     for dim in range(acceleration.shape[1]):
-        acceleration[:, dim] /= g
-        voltage = np.dot(acceleration[:, dim], sensitivity)
+        voltage = acceleration[:, dim]*sensitivity[dim]
         convolved_list.append(signal.convolve(voltage, linear_response))
     convolved_signal = np.array(convolved_list).T
 
