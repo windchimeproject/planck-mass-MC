126871c715f26c2a2b7a486f759d8d9bc344972e
belshimy <51716656+belshimy@users.noreply.github.com>
2020-07-27 13:30:19 -0700

    Add files via upload

--- generate_linear_response_json.py
+++ generate_linear_response_json.py
@@ -6,14 +6,24 @@ import argparse
 import json
 
 import numpy as np
+from scipy.signal import kaiserord, firwin
+from planckmc.config import CONFIG
 from planckmc.detector_characteristics import DETECTOR_CHARACTERISTICS
 
-def output_response_json(file, bits, min_voltage, max_voltage):
+def FIR_filter(sample_rate = 1 / (float(CONFIG['Track Generation']['Timestep']) * 10**(-9)), ripple_db = 20.0, cutoff_hz = 50000):
+    nyq_rate = sample_rate / 2.0
+    width = 10000.0/nyq_rate
+    N, beta = kaiserord(ripple_db, width)
+    taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
+    return taps
+
+def output_response_json(file, bits, min_acceleration, max_acceleration):
     '''generate response json'''
-    bin_borders = list(np.linspace(min_voltage, max_voltage, 2**bits-1, endpoint=True))
+    bin_borders = list(np.linspace(min_acceleration, max_acceleration, 2**bits-1, endpoint=True))
     output_dict = {}
+    Lin_Resp = list(FIR_filter())
     for sensor in DETECTOR_CHARACTERISTICS:
-        output_dict[sensor] = {'linear_response': [1], 'signal_transfer_response': bin_borders}
+        output_dict[sensor] = {'linear_response': Lin_Resp, 'signal_transfer_response': bin_borders}
     json.dump(output_dict, file)
 
 
@@ -23,8 +33,8 @@ if __name__ == '__main__':
                                      'not to emulate a real experiment!')
     PARSER.add_argument('output', type=str)
     PARSER.add_argument('bits', type=int)
-    PARSER.add_argument('min_voltage', type=float)
-    PARSER.add_argument('max_voltage', type=float)
+    PARSER.add_argument('min_acceleration', type=float)
+    PARSER.add_argument('max_acceleration', type=float)
     ARGS = PARSER.parse_args()
     with open(ARGS.output, 'w') as f:
-        output_response_json(f, ARGS.bits, ARGS.min_voltage, ARGS.max_voltage)
+        output_response_json(f, ARGS.bits, ARGS.min_acceleration, ARGS.max_acceleration)
