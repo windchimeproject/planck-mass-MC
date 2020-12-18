6d78fec588be2619c04cd93b5c06580cfb5a2325
belshimy <belshimy@users.noreply.github.com>
2020-12-17 20:58:26 -0500

    Update generate_linear_response_json.py

--- generate_linear_response_json.py
+++ generate_linear_response_json.py
@@ -17,38 +17,38 @@ def FIR_filter(sample_rate = 1 / (float(CONFIG['Track Generation']['Timestep']) 
     taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
     return taps
 
-def FIR_filter_Lin_Tri(desired_len = 100, max_val = 0.45):
+def FIR_filter_Lin_Tri(desired_len, max_val):
     #len actually is: desired_len plus 1 if it is even, exactly equal to desired_len if it is odd
 
-    dum_init = np.linspace(0, max_val, int((desired_len - 2) / 2))
-    dum_fin = np.linspace(max_val, 0, int((desired_len - 2) / 2))
-    actl_max = [((max_val / (int(desired_len - 2) / 2))) + max_val]
+    side_sum = (1 + max_val) / 2
+    constant = side_sum / max_val
+    num_of_vals = round(2 * constant)
 
-    if len(dum_init) + len(dum_fin) != desired_len:
-        side_pad_FIR = int((desired_len - (len(dum_init) + len(dum_fin))) / 2)
-        init_array, fin_array = np.zeros(side_pad_FIR), np.zeros(side_pad_FIR)
-        arr_lin_resp_long = np.concatenate((init_array, dum_init, actl_max, dum_fin, fin_array))
-        arr_lin_resp = arr_lin_resp_long / np.linalg.norm(arr_lin_resp_long, ord=1)
+    init_array = np.linspace(0, max_val, num_of_vals)[0:-1]
+    end_array = np.linspace(max_val, 0, num_of_vals)[1:]
+    dum_side_array = np.zeros(int((desired_len - (len(init_array) + len(end_array))) / 2))
+    arr_lin_resp = np.concatenate((dum_side_array, init_array, [max_val], end_array, dum_side_array))
     return arr_lin_resp
 
-def FIR_filter_Gauss(centr_gauss = 0, sigma_gauss = 0.25, desired_len_guass = 100):
-    x_guass = np.linspace(-sigma_gauss * 4, sigma_gauss * 4, desired_len_guass)
+def FIR_filter_Gauss(centr_gauss, sigma_gauss, desired_len):
+    x_guass = np.linspace(-sigma_gauss * 4, sigma_gauss * 4, desired_len)
     arr_lin_resp_long_gauss = (2 * np.pi * sigma_gauss ** 2) ** -.5 * np.exp(
         -.5 * (x_guass - centr_gauss) ** 2 / sigma_gauss ** 2)
 
     arr_lin_resp_gauss = arr_lin_resp_long_gauss / np.linalg.norm(arr_lin_resp_long_gauss, ord=1)
     return arr_lin_resp_gauss
 
-def output_response_json(file, bits, min_acceleration, max_acceleration, Resp = 'FIR_filter'):
+def output_response_json(file, bits, min_acceleration, max_acceleration, desired_len = 801, max_val = 0.5, centr_gauss = 0, sigma_gauss = 0.25, Resp = 'FIR_filter'):
     '''generate response json'''
     bin_borders = list(np.linspace(min_acceleration, max_acceleration, 2**bits-1, endpoint=True))
     output_dict = {}
     if Resp == 'FIR_filter':
         Lin_Resp = list(FIR_filter())
     elif Resp == 'Tri_filter':
-        Lin_Resp = list(FIR_filter_Lin_Tri())
+        Lin_Resp = list(FIR_filter_Lin_Tri(desired_len, max_val))
     elif Resp == 'Gauss_filter':
-        Lin_Resp = list(FIR_filter_Gauss)
+        Lin_Resp = list(FIR_filter_Gauss(centr_gauss, sigma_gauss, desired_len))
+
     for sensor in DETECTOR_CHARACTERISTICS:
         output_dict[sensor] = {'linear_response': Lin_Resp, 'signal_transfer_response': bin_borders}
     json.dump(output_dict, file)
